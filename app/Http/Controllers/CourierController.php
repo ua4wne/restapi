fff2d60ca78c466934fa2f2d5673e96d78061873
dervish <rai68@yandex.ru>
2019-05-05 12:04:55 +0300

    начало работы

---
+++ app/Http/Controllers/CourierController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Courier;
+use Illuminate\Http\Request;
+
+class CourierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return 'REST index';
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Courier  $courier
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Courier $courier)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Courier  $courier
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Courier $courier)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Courier  $courier
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Courier $courier)
+    {
+        //
+    }
+}
